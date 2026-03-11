51329cdf923785aebe127881fff86f125eab9844
Seyid-Code <seyidahmadhb-apa103@code.edu.az>
2026-03-11 23:08:47 +0400

    Tasks

---
+++ 01-C#IntroMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_C_IntroMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
