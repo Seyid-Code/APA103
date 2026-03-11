0202ad467d3ad5c288baa451f26e3a8cb276eb15
Seyid-Code <seyidahmadhb-apa103@code.edu.az>
2026-03-11 23:59:58 +0400

    Tasks

--- 01-C#IntroMethods/Program.cs
+++ 01-C#IntroMethods/Program.cs
@@ -1,10 +1,116 @@
-﻿namespace _01_C_IntroMethods
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace _01_C_IntroMethods
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//1-ci Task
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Birinci ededi daxil edin:");
+//        double a = Convert.ToDouble(Console.ReadLine());
+//        Console.WriteLine("Ikinci ededi daxil edin:");
+//        double b = Convert.ToDouble(Console.ReadLine());
+//        RiyaziEmel(a, b);
+//    }
+//    static void RiyaziEmel(double a, double b)
+//    {
+//        Console.WriteLine("Toplama: " + (a + b));
+//        Console.WriteLine("Cixma: " + (a - b));
+//        Console.WriteLine("Vurma: " + (a * b));
+//        if (b != 0)
+//            Console.WriteLine("Bolme: " + (a / b));
+//        else
+//            Console.WriteLine("0-a bolmek olmaz");
+//    }
+//}
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+//2-ci Task
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        int[] arr = { 14, 20, 35, 40, 57, 60, 100 };
+//        Eded(arr);
+//    }
+
+//    static void Eded(int[] arr)
+//    {
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            if (arr[i] % 2 == 0)
+//                Console.WriteLine(arr[i] + " Cut Ededdir");
+//            else
+//                Console.WriteLine(arr[i] + " Tek Ededdir");
+//        }
+//    }
+//}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//3-cu Task
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        int[] arr = { 14, 20, 35, 40, 57, 60, 100 };
+//        int netice = CemTap(arr);
+//        Console.WriteLine("Cem: " + netice);
+//    }
+//    static int CemTap(int[] arr)
+//    {
+//        int cem = 0;
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            if (arr[i] % 4 == 0 && arr[i] % 5 == 0)
+//            {
+//                cem += arr[i];
+//            }
+//        }
+//        return cem;
+//    }
+//}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//4-cu Task
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Cumleni Daxil Edin: ");
+//        string cumle = Console.ReadLine();
+//        Console.Write("Simvolu Daxil Edin: ");
+//        char simvol = Convert.ToChar(Console.ReadLine());
+//        int say = 0;
+//        for (int i = 0; i < cumle.Length; i++)
+//        {
+//            if (Char.ToLower(cumle[i]) == Char.ToLower(simvol))
+//            {
+//                say++;
+//            }
+//        }
+//        Console.WriteLine("Simvolun Sayi: " + say);
+//    }
+//}
+
+//-----------------------------------------------------------------------------------------------------------------------
